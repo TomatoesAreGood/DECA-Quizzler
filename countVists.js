27631b84d2b34b0b75fab2e0dc72726c4fb8d817
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2024-12-25 13:37:06 -0500

    update

--- countVists.js
+++ countVists.js
@@ -1,8 +1,10 @@
 
-const vistsKey = "decaquizzler123";
-const uniqueVistsKey = "decaquizzlerunique123";
+const visitsKey = "decaquizzler1234";
+const visitsAdminKey = "cd0accd9-8c3d-4de6-88bd-92d749a909f2";
+const uniqueVisitsKey = "decaquizzlerunique1234";
+const uniqueVisitsAdminKey = "fa20af18-1004-45ce-a031-114d21bfc3de";
 
-fetch(`https://abacus.jasoncameron.dev/hit/${vistsKey}/`)
+fetch(`https://abacus.jasoncameron.dev/hit/${visitsKey}/`)
     .then( response => {
         if(!response.ok){
             throw new Error("API error");
@@ -13,15 +15,54 @@ fetch(`https://abacus.jasoncameron.dev/hit/${vistsKey}/`)
 });
  
 
-if(localStorage.getItem("uniqueVist") == null){
-    fetch(`https://abacus.jasoncameron.dev/hit/${uniqueVistsKey}/`)
+if(localStorage.getItem("uniqueVisit") === null){
+    fetch(`https://abacus.jasoncameron.dev/hit/${uniqueVisitsKey}/`)
         .then( response => {
             if(!response.ok){
                 throw new Error("API error");
             }
-            localStorage.setItem("uniqueVist", "True");
+            localStorage.setItem("uniqueVisit", "True");
         }
     ).catch(error => {
         console.error(error);
     });
 }
+fetch(`https://abacus.jasoncameron.dev/set/${visitsKey}?value=15`, {
+    method: "POST",
+    body: JSON.stringify({
+    //   "value": 10,
+        "Authorization": `Bearer ${visitsAdminKey}`
+    }), 
+    // headers: {
+    //   "Authorization": `Bearer ${visitsAdminKey}`
+    // }
+  }).then( response => {
+    console.log(response.status);
+});   
+
+
+// fetch("visits.json")
+//     .then(response => response.json())
+//         .then(data => {
+//             const date = new Date();
+//             const currentWeekday = date.getDay();
+
+//             const weeklyVisits = data["weeklyVisits"];
+//             const weekday = data["weekday"];
+
+//             if(currentWeekday !== weekday){
+
+//             }
+
+//             if(localStorage.getItem("lastUpdate") === null){
+    
+//                 localStorage.setItem("lastUpdate", date.getDate());
+            
+//                 // localStorage.setItem("lastUpdate", data.toString());
+//             }else{
+//                 const lastUpdate = localStorage.getItem("lastUpdate");
+//             }
+
+//         }
+//     );
+

491727cf0005205fae3fd315fa1f1df5dcbbf82c
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2025-01-15 19:51:20 -0500

    new exams + popNavBar refactor

--- populateNavBar.js
+++ populateNavBar.js
@@ -1,178 +1,15 @@
-const allDropdowns = document.querySelectorAll(".dropdown .grid-container");
-const entDropdown = allDropdowns[0];
-const finDropdown = allDropdowns[1];
-const mktDropdown = allDropdowns[2];
-const hosptDropdown = allDropdowns[3];
-const bmaDropdown = allDropdowns[4];
-const coreDropdown = allDropdowns[5];
-
-async function fetchEnt(){
-    if(sessionStorage.getItem("ENT") != null){
-        let storedExams = sessionStorage.getItem("ENT").split(',');
-        for (const item of storedExams){
-            entDropdown.innerHTML += `
-                <li class="dropdown-element">
-                    <a href = "quiz.html?&exam=${item}">${item}</a>
-                </li>
-            `;    
-        }
-        allExams = allExams.concat(storedExams);
-    }else{
-        fetch("data/ENT.json")
-        .then(response => response.json())
-        .then(data => {
-            let exams = Object.keys(data);
-            exams.push("9999-ENT-UNIT");
-
-            for(const key of exams){   
-                entDropdown.innerHTML += `
-                    <li class="dropdown-element">
-                        <a href = "quiz.html?exam=${key}">${key}</a>
-                    </li>
-                `;
-            }
-            allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("ENT", exams);
-        });
-    }
-}
-
-async function fetchFin(){
-    if(sessionStorage.getItem("FIN") != null){
-        let storedExams = sessionStorage.getItem("FIN").split(',');
-        for (const item of storedExams){
-            finDropdown.innerHTML += `
-                <li class="dropdown-element">
-                    <a href = "quiz.html?&exam=${item}">${item}</a>
-                </li>
-            `;     
-        }
-        allExams = allExams.concat(storedExams);
-    }else{
-        fetch("data/FIN.json")
-        .then(response => response.json())
-        .then(data => {
-            let exams = Object.keys(data);
-            exams.push("9999-FIN-UNIT");
-
-            for(const key of exams){
-                finDropdown.innerHTML += `
-                    <li class="dropdown-element">
-                        <a href = "quiz.html?exam=${key}">${key}</a>
-                    </li>
-                `;
-            }
-            allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("FIN", exams);
-        });
-    }
-}
-
-async function fetchMkt(){
-    if(sessionStorage.getItem("MKT") != null){
-        let storedExams = sessionStorage.getItem("MKT").split(',');
-        for (const item of storedExams){
-            mktDropdown.innerHTML += `
-                <li class="dropdown-element">
-                    <a href = "quiz.html?&exam=${item}">${item}</a>
-                </li>
-            `;     
-        }
-        allExams = allExams.concat(storedExams);
-    }else{
-        fetch("data/MKT.json")
-        .then(response => response.json())
-        .then(data => {
-            let exams = Object.keys(data);
-            exams.push("9999-MKT-UNIT");
-
-            for(const key of exams){
-                mktDropdown.innerHTML += `
-                    <li class="dropdown-element">
-                        <a href = "quiz.html?exam=${key}">${key}</a>
-                    </li>
-                `;
-            }
-            allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("MKT", exams);
-        });
-    }
-}
-
-async function fetchHnt(){
-    if(sessionStorage.getItem("H&T") != null){
-        let storedExams = sessionStorage.getItem("H&T").split(',');
-        for (const item of storedExams){
-            hosptDropdown.innerHTML += `
-                <li class="dropdown-element">
-                    <a href = "quiz.html?&exam=${item}">${item.substring(0,4)}-H&T</a>
-                </li>
-            `;
-        }
-        allExams = allExams.concat(storedExams);
-    }else{
-        fetch("data/HnT.json")
-        .then(response => response.json())
-        .then(data => {
-            let exams = Object.keys(data);
-            exams.push("9999-HnT-UNIT");
-
-            for(const key of exams){
-                hosptDropdown.innerHTML += `
-                    <li class="dropdown-element">
-                        <a href = "quiz.html?exam=${key}">${key.substring(0,4)}-H&T</a>
-                    </li>
-                `;
-            }
-            allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("H&T", exams);
-        });
-    }
-}
-
-async function fetchBma(){
-    if(sessionStorage.getItem("BMA") != null){
-        let storedExams = sessionStorage.getItem("BMA").split(',');
-        for (const item of storedExams){
-            bmaDropdown.innerHTML += `
-                <li class="dropdown-element">
-                    <a href = "quiz.html?&exam=${item}">${item}</a>
-                </li>
-            `;
-        }
-        allExams = allExams.concat(storedExams);
-    }else{
-        fetch("data/BMA.json")
-        .then(response => response.json())
-        .then(data => {
-            let exams = Object.keys(data);
-            exams.push("9999-BMA-UNIT");
-
-            for(const key of exams){
-                bmaDropdown.innerHTML += `
-                    <li class="dropdown-element">
-                        <a href = "quiz.html?exam=${key}">${key}</a>
-                    </li>
-                `;
-            }
-            allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("BMA", exams);
-        });
-    }
-}
-
-async function fetchCore(){
-    if(sessionStorage.getItem("CORE") != null){
-        let storedExams = sessionStorage.getItem("CORE").split(',');
+async function fetchExam(sector, dropdown){
+    if(sessionStorage.getItem(sector) != null){
+        let storedExams = sessionStorage.getItem(sector).split(',');
         for (const item of storedExams){
             if(item.length > 15){
-                coreDropdown.innerHTML += `
+                dropdown.innerHTML += `
                 <li class="dropdown-element">
                     <a href = "quiz.html?&exam=${item}">${item.substring(0,13)}...</a>
                 </li>
             `; 
             }else{
-                coreDropdown.innerHTML += `
+                dropdown.innerHTML += `
                 <li class="dropdown-element">
                     <a href = "quiz.html?&exam=${item}">${item}</a>
                 </li>
@@ -181,21 +18,21 @@ async function fetchCore(){
         }
         allExams = allExams.concat(storedExams);
     }else{
-        fetch("data/CORE.json")
+        fetch(`data/${sector}.json`)
         .then(response => response.json())
         .then(data => {
             let exams = Object.keys(data);
-            exams.push("9999-CORE-UNIT");
+            exams.push(`9999-${sector}-UNIT`);
 
             for(const item of exams){
                 if(item.length > 15){
-                    coreDropdown.innerHTML += `
+                    dropdown.innerHTML += `
                     <li class="dropdown-element">
                         <a href = "quiz.html?&exam=${item}">${item.substring(0,13)}...</a>
                     </li>
                 `; 
                 }else{
-                    coreDropdown.innerHTML += `
+                    dropdown.innerHTML += `
                     <li class="dropdown-element">
                         <a href = "quiz.html?&exam=${item}">${item}</a>
                     </li>
@@ -203,7 +40,7 @@ async function fetchCore(){
                 }
             }
             allExams = allExams.concat(Array.from(exams));
-            sessionStorage.setItem("CORE", exams);
+            sessionStorage.setItem(sector, exams);
         });
     }
 }
@@ -267,8 +104,16 @@ const sidebar = document.querySelector(".sidebar");
 const resultBox = document.querySelector(".result-box");
 const inputBox = document.getElementById("input")
 
+const allDropdowns = document.querySelectorAll(".dropdown .grid-container");
+const entDropdown = allDropdowns[0];
+const finDropdown = allDropdowns[1];
+const mktDropdown = allDropdowns[2];
+const hosptDropdown = allDropdowns[3];
+const bmaDropdown = allDropdowns[4];
+const coreDropdown = allDropdowns[5];
+
 let allExams = [];
-const fetchExams = [fetchEnt(),fetchFin(),fetchMkt(),fetchHnt(),fetchBma(),fetchCore()];
+const fetchExams = [fetchExam("ENT", entDropdown),fetchExam("FIN", finDropdown),fetchExam("MKT", mktDropdown),fetchExam("HnT", hosptDropdown),fetchExam("BMA", bmaDropdown),fetchExam("CORE", coreDropdown)];
 
 
 function searchResult(){

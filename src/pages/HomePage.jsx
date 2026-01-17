8a0c34a78a5768bcf3c87412a3c8504fb6623efc
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2026-01-16 19:52:49 -0500

    visual changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,8 @@ export function HomePage() {
     <main>
       <div className="page-header">
         <h1>DECA Quizzler</h1>
-        <p className="header-subtext-md">A minimalistic DECA quizzing website</p>
+        <p className="header-subtext-xl">A minimalistic DECA quizzing website</p>
       </div>
-      <h2 id="sectorHeading">Sectors</h2>
       <SectorGrid />
     </main>
   );
